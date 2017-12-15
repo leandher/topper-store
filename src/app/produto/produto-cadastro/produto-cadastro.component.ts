4d450f2e3804a6c583d370f07abb98e4bf6d7c8e
João Victor Mendes Ávila <joaovictormendesavila@gmail.com>
2017-12-14 23:05:51 -0300

    Auto stash before merge of "master" and "origin/master"

--- src/app/produto/produto-cadastro/produto-cadastro.component.ts
+++ src/app/produto/produto-cadastro/produto-cadastro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProdutoComponent } from '../produto.component';
+import { ProdutoService } from '../produto.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-produto-cadastro',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./produto-cadastro.component.css']
 })
 export class ProdutoCadastroComponent implements OnInit {
-
-  constructor() { }
+  produto: ProdutoComponent = new ProdutoComponent;
+  mensagem: string = "";
+  style: string = "";
+  constructor(private service: ProdutoService, private router: ActivatedRoute) { }
 
   ngOnInit() {
   }
+  cadastrar() {
+    console.log(this.produto);
 
+    this.service.cadastrar(this.produto).subscribe(res => {
+      this.mensagem = "Salvo com sucesso!";
+      this.style = 'success';
+      setInterval(() => {
+        this.mensagem = "";
+      }, 1000 * 3);
+    }, err => {
+      this.mensagem = "E-mail e/ou CPF já cadastrados!";
+      this.style = 'danger';
+      setInterval(() => {
+        this.mensagem = "";
+      }, 1000 * 5);
+    })
+  }
 }
